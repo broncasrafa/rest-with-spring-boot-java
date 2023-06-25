85904f052c532befe9f66b52176e4c1a5b10ee65
Rafael Francisco <broncasrafa@gmail.com>
2023-06-25 17:56:39 -0300

    testing modelmapper library

--- src/main/java/com/rsfrancisco/restwithspringbootjava/application/mappers/Mapper.java
+++ src/main/java/com/rsfrancisco/restwithspringbootjava/application/mappers/Mapper.java
@@ -1,17 +1,21 @@
 package com.rsfrancisco.restwithspringbootjava.application.mappers;
 
 import com.github.dozermapper.core.DozerBeanMapperBuilder;
+import org.modelmapper.ModelMapper;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Mapper {
     private static com.github.dozermapper.core.Mapper _mapper = com.github.dozermapper.core.DozerBeanMapperBuilder.buildDefault();
-    public static <Source, Destination> Destination mapper(Source source, Class<Destination> destination){
+    //private static ModelMapper _mapper = new ModelMapper();
+
+
+    public static <Source, Destination> Destination mapper(Source source, Class<Destination> destination) {
         return _mapper.map(source, destination);
     }
 
-    public static <Source, Destination> List<Destination> mapper(List<Source> sourceList, Class<Destination> destination){
+    public static <Source, Destination> List<Destination> mapper(List<Source> sourceList, Class<Destination> destination) {
         List<Destination> destinationList = new ArrayList<>();
         for(Source source : sourceList) {
             destinationList.add(_mapper.map(source, destination));
